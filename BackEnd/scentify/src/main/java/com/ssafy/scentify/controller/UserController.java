0968a24bba6cc69a458fb896b51ffea18becd1ac
SunJung <djtjswjd1@naver.com>
2025-01-20 18:05:33 +0900

    [FIX] conflict 해결

--- BackEnd/scentify/src/main/java/com/ssafy/scentify/controller/UserController.java
+++ BackEnd/scentify/src/main/java/com/ssafy/scentify/controller/UserController.java
@@ -2,10 +2,11 @@ package com.ssafy.scentify.controller;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import lombok.extern.log4j.Log4j2;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
-
+@Log4j2
 @RequestMapping("/v1/user")
 @RestController
 public class UserController {
---
+++ BackEnd/scentify/src/main/java/com/ssafy/scentify/model/entity/Device.java
@@ -0,0 +1,31 @@
+package com.ssafy.scentify.model.entity;
+
+import lombok.*;
+
+@Getter
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+public class Device {
+    private Integer id;
+    private String serial;
+    private String name;
+    private String userId;
+    private Integer groupId;
+    private String ipAddress;
+    private Integer roomType;
+    private Integer slot1;
+    private Integer slot1RemainingRatio;
+    private Integer slot2;
+    private Integer slot2RemainingRatio;
+    private Integer slot3;
+    private Integer slot3RemainingRatio;
+    private Integer slot4;
+    private Integer slot4RemainingRatio;
+    private Boolean mode;
+    private Float temperature;
+    private Integer humidity;
+    private Integer defaultCombination;
+
+}
+
