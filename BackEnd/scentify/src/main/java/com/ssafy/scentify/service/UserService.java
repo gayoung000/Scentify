17df3899cebaf0fbd66c3be85ebda2fe9c8646c4
어선정 <sunjung.eo@gmail.com>
2025-01-20 21:43:27 +0900

    [FEAT] S12P11A205-109 ID 중복확인 구현

--- BackEnd/scentify/src/main/java/com/ssafy/scentify/service/UserService.java
+++ BackEnd/scentify/src/main/java/com/ssafy/scentify/service/UserService.java
@@ -1,5 +1,23 @@
 package com.ssafy.scentify.service;
 
+import org.springframework.stereotype.Service;
+import com.ssafy.scentify.model.entity.*;
+import com.ssafy.scentify.model.repository.UserRepository;
+import lombok.extern.log4j.Log4j2;
+
+@Log4j2
+@Service
 public class UserService {
+	
+	private final UserRepository userRepository;
+	
+	public UserService(UserRepository userRepository) {
+		this.userRepository = userRepository;
+	}
+
+	public User selectUserById(String id) {
+	    User user = userRepository.selectUserById(id);
+		return user != null ? user : null;
+	}
 
 }
