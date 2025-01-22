2e4de2a089f49c50b229810a4184d2ca75499f0e
seof622 <seof622@naver.com>
2025-01-22 18:23:56 +0900

    feat: S12P11A205-139 complete getting raspberry pi serial number test
    Former-commit-id: 8190b847c1a4e2f3a06bf458da205fa83cd16937

--- Embedded/Websocket/src/websocket_client.py
+++ Embedded/Websocket/src/websocket_client.py
@@ -1,26 +1,46 @@
 import asyncio
 import websockets
 import hashlib
+import subprocess
 import base64
 
-# 연결 테스트 코드
-async def test_websocket_connection():
-    # uri = "ws://localhost:8765/v1/ws/device"  # 서버의 주소와 포트로 수정
-    uri = "ws://0.0.0.0:8765"
+class WebSocketClient:
+    def __init__(self, uri, serial_number):
+        # 서버의 주소와 포트로 수정
+        # self.uri = "ws://0.0.0.0:8765"
+        self.__uri = uri
 
-    # 인증을 위한 시리얼 넘버 해싱 (SHA-256)
-    serial_number = "f5tH8jW9qA2D1Z7n"  # 실제 시리얼 넘버로 변경
-    token = hashlib.sha256(serial_number.encode()).hexdigest()
+        # 인증을 위한 시리얼 넘버 해싱 (SHA-256)
+        # self.serial_number = "f5tH8jW9qA2D1Z7n"
+        self.__serial_number = serial_number
 
-    headers = {
-        "Authorization": f"Bearer {token}"
-    }
+    # 연결 테스트 코드
+    async def test_websocket_connection(self, ):
+        
+        token = hashlib.sha256(self.__serial_number.encode()).hexdigest()
 
-    async with websockets.connect(uri, extra_headers=headers) as websocket:
-        print("웹소켓 연결 성공!")
-        while True:
-            response = await websocket.recv()
-            print(f"서버로부터 받은 메시지: {response}")
+        headers = {
+            "Authorization": f"Bearer {token}"
+        }
 
-# 비동기 이벤트 루프 실행
-asyncio.run(test_websocket_connection())
+        async with websockets.connect(self.__uri, extra_headers=headers) as websocket:
+            print("웹소켓 연결 성공!")
+            while True:
+                response = await websocket.recv()
+                print(f"서버로부터 받은 메시지: {response}")
+
+
+if __name__ == '__main__':
+    # 라즈베리파이 시리얼 넘버 파싱
+    serial_number = subprocess.run(
+        "cat /proc/cpuinfo | grep Serial | awk '{print $3}'",
+        shell=True,
+        capture_output=True,
+        text=True
+    ).stdout.strip()
+
+    # 웹 소켓 객체 생성
+    websocket_client = WebSocketClient("ws://0.0.0.0:8765", serial_number)
+
+    # 비동기 이벤트 루프 실행
+    asyncio.run(websocket_client.test_websocket_connection())
