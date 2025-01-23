8da0b6221b88819c42bf24c9f0ee434a7b70a0f6
주현호 <sparrow9576@naver.com>
2025-01-23 15:22:31 +0900

    ✨ feat: [Control] 모드 변경 토글 <주현호>

---
+++ FrontEnd/scentify/src/feature/control/main/ModeToggle.tsx
@@ -0,0 +1,24 @@
+import { ModeToggleProps } from "./ControlType";
+import "../../../styles/global.css";
+
+// 예약 모드 / 자동화 모드 토글
+export default function ModeToggle({ onModeChange, mode }: ModeToggleProps) {
+  return (
+    <div>
+      <div className="flex bg-white rounded-full border border-brand font-pre-light text-xs">
+        <div
+          onClick={() => onModeChange(false)}
+          className={`flex-1 py-2 text-center transition-all rounded-full ${mode === true ? "text-black" : "bg-brand text-white"}`}
+        >
+          예약 모드
+        </div>
+        <div
+          onClick={() => onModeChange(true)}
+          className={`flex-1 py-2 text-center transition-all rounded-full ${mode === false ? "text-black" : "bg-brand text-white"}`}
+        >
+          자동화 모드
+        </div>
+      </div>
+    </div>
+  );
+}
--- FrontEnd/scentify/src/pages/Control/Control.tsx
+++ FrontEnd/scentify/src/pages/Control/Control.tsx
@@ -1,10 +1,28 @@
+import { useState } from "react";
+import { Mode } from "../../feature/control/main/ControlType";
+import ModeToggle from "../../feature/control/main/ModeToggle";
+import "../../styles/global.css";
+
 const Control = () => {
-    return (
-      <div className="flex items-center justify-center h-full">
-        <h1 className="text-3xl font-bold">Control</h1>
-      </div>
-    );
+  const [mode, setMode] = useState<Mode>(false);
+  // 다른 모드 클릭 시 보여지는 화면 변경
+  const handleModeChangeRequest = (currentMode: Mode) => {
+    setMode(currentMode);
   };
-  
+
+  return (
+    <div className="content">
+      <div className="flex flex-col w-full px-4">
+        <div>
+          <h2 className="font-pre-medium text-xl mb-4">모드 설정</h2>
+          <ModeToggle onModeChange={handleModeChangeRequest} mode={mode} />
+        </div>
+        <div className="mt-16 font-pre-medium text-xl">
+          {mode === false ? <h1>예약 관리</h1> : <h2>자동화 모드 설정</h2>}
+        </div>
+      </div>
+    </div>
+  );
+};
+
 export default Control;
-  
