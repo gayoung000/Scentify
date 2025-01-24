655ee3724b4396e414d1bc6cc3a6d703f57c458f
주현호 <sparrow9576@naver.com>
2025-01-24 10:21:10 +0900

    ✨ feat: [Control] 예약 관리 컴포넌트 UI, 로직 구현 <주현호>

--- FrontEnd/scentify/src/components/DeviceSelect.tsx
+++ FrontEnd/scentify/src/components/DeviceSelect.tsx
@@ -1,11 +1,19 @@
 import { useState } from "react";
+import ArrowDownIcon from "../assets/icons/arrow-down-icon.svg";
+
+// 기기 선택 타입입
+interface DeviceSelectProps {
+  devices: string[];
+  selectedDevice: string;
+  onDeviceChange: (device: string) => void;
+}
 
 // 기기 선택
 export default function DeviceSelect({
   devices,
   selectedDevice,
   onDeviceChange,
-}) {
+}: DeviceSelectProps) {
   const [isOpen, setIsOpen] = useState(false);
 
   return (
@@ -15,12 +23,11 @@ export default function DeviceSelect({
         className="flex w-[120px] h-[36px] p-3 border border-lightgray font-pre-light text-sm items-center justify-between rounded-lg"
       >
         {selectedDevice}
-        {/* svg로 대체체 */}
-        <p>▼</p>
+        <img src={ArrowDownIcon} alt="아래 화살표 이미지" />
       </button>
       {isOpen && (
         <div className="w-[120px] h-[36px] font-pre-light text-sm">
-          <div className="border border-lightgray rounded-lg">
+          <div className="border border-lightgray bg-white rounded-lg">
             {devices.map((device) => (
               <div
                 key={device}
--- FrontEnd/scentify/src/feature/control/reservation/DeleteReservationModal.tsx
+++ FrontEnd/scentify/src/feature/control/reservation/DeleteReservationModal.tsx
@@ -1,21 +1,13 @@
-import { ModeChangeModalProps } from "./ControlType";
+import { DeleteModalProps } from "./ReservationType";
 
-// 모드 변경 모달창
-export default function ModeChangeModal({
-  nextMode,
+export default function DeleteConfirmModal({
   onConfirm,
   onCancel,
-}: ModeChangeModalProps) {
-  const getModeName = () => {
-    return nextMode === false ? "예약 " : "자동화 ";
-  };
-
+}: DeleteModalProps) {
   return (
     <div className="fixed inset-0 flex justify-center items-center z-50 p-6">
       <div className="flex flex-col w-[300px] h-[200px] p-10 bg-component font-pre-light text-sm rounded-lg">
-        <p className="text-center mb-6">
-          {getModeName()}모드로 변경하시겠습니까?
-        </p>
+        <p className="text-center mb-6">예약을 삭제하시겠습니까?</p>
         <div className="flex mt-auto justify-between">
           <button
             onClick={onConfirm}
