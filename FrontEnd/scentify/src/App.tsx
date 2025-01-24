801495a34eff9daa80acb5c28a87a318d3cf1ec3
leegayoung <leegy0720@naver.com>
2025-01-24 16:50:09 +0900

    💄 stlye: S12P11A205-122 [user] 회원가입 페이지 UI 완성 및 Header.tsx 업데이트 <이가영>

--- FrontEnd/scentify/src/App.tsx
+++ FrontEnd/scentify/src/App.tsx
@@ -1,44 +1,50 @@
-import React, { useEffect } from "react";
+import React, { useEffect } from 'react';
 import {
   BrowserRouter as Router,
   Routes,
   Route,
   Navigate,
-} from "react-router-dom";
-import { useAuthStore } from "./stores/useAuthStore.ts";
-import Layout from "./layout/Layout.tsx";
-import Home from "./pages/Home/Home.tsx";
-import Control from "./pages/Control/Control.tsx";
-import Scent from "./pages/Scent/Scent.tsx";
-import My from "./pages/My/My.tsx";
-import Start from "./pages/Start/start.tsx";
-import Login from "./pages/Login/Login.tsx";
+} from 'react-router-dom';
+import { useAuthStore } from './stores/useAuthStore.ts';
+import Layout from './layout/Layout.tsx';
+import Home from './pages/Home/Home.tsx';
+import Control from './pages/Control/Control.tsx';
+import Scent from './pages/Scent/Scent.tsx';
+import My from './pages/My/My.tsx';
+import Start from './pages/Start/start.tsx';
+import Login from './pages/Login/Login.tsx';
+import Regist from './feature/user/register/Regist.tsx';
 
-import UserRoutes from "./feature/user/UserRoutes.tsx";
-import "./styles/global.css";
+import UserRoutes from './feature/user/UserRoutes.tsx';
+import './styles/global.css';
 
+// 실제 뷰포트 높이를 기준으로 CSS 변수 설정
 const setScreenSize = () => {
-  // 실제 뷰포트 높이를 기준으로 CSS 변수 설정
   const vh = window.innerHeight * 0.01;
-  document.documentElement.style.setProperty("--vh", `${vh}px`);
+  document.documentElement.style.setProperty('--vh', `${vh}px`);
 };
+
 function App() {
   const { isAuthenticated } = useAuthStore();
 
   useEffect(() => {
     setScreenSize(); // 초기 로드 시 실행
-    window.addEventListener("resize", setScreenSize); // 창 크기 변경 시 업데이트
+    window.addEventListener('resize', setScreenSize); // 창 크기 변경 시 업데이트
 
     return () => {
-      window.removeEventListener("resize", setScreenSize); // 컴포넌트 언마운트 시 제거
+      window.removeEventListener('resize', setScreenSize); // 컴포넌트 언마운트 시 제거
     };
   }, []);
 
   return (
     <Router>
       <Routes>
+        {/* 시작 페이지, 로그인 페이지지 */}
         <Route path="/" element={<Start />} />
         <Route path="/auth/login" element={<Login />} />
+
+        {/* 회원가입 페이지 - 독립적인 페이지로 처리 */}
+        <Route path="/user/regist" element={<Regist />} />
 
         <Route
           path="/user/*"
@@ -48,7 +54,6 @@ function App() {
             </Layout>
           }
         />
-        {/* <Route path="/user/:id" element={<UserDetail />} /> */}
 
         {/* 인증 여부에 따른 라우팅 처리 */}
         {isAuthenticated ? (
--- FrontEnd/scentify/src/layout/Header.tsx
+++ FrontEnd/scentify/src/layout/Header.tsx
@@ -1,8 +1,9 @@
-import React from "react";
-import Logo from "../assets/icons/scentify-green-logo.svg?react";
-import FinishButton from "../components/Button/Button";
-import BackBtn from "../assets/icons/back-arrow-btn.svg?react";
-import AddBtn from "../assets/icons/add-btn.svg?react";
+import React from 'react';
+import { useNavigate, useLocation } from 'react-router-dom';
+import Logo from '../assets/icons/scentify-green-logo.svg?react';
+import FinishButton from '../components/Button/Button';
+import BackBtn from '../assets/icons/back-arrow-btn.svg?react';
+import AddBtn from '../assets/icons/add-btn.svg?react';
 
 interface HeaderProps {
   showBack: boolean;
@@ -17,10 +18,22 @@ const Header = ({
   showDeviceManage,
   showAdd,
 }: HeaderProps) => {
+  const navigate = useNavigate();
+  const location = useLocation();
+
+  // 뒤로가기 핸들러:
+  const handleBackClick = () => {
+    if (window.history.length > 1) {
+      navigate(-1); // 기본적으로 이전 페이지로 이동
+    } else {
+      navigate('/auth/login'); // 만약 이전 페이지가 없으면 로그인 페이지로 이동
+    }
+  };
+
   return (
     <header className="header flex w-full flex-row items-center justify-between px-5">
       {showBack ? (
-        <button onClick={() => window.history.back()}>
+        <button onClick={handleBackClick}>
           <BackBtn />
         </button>
       ) : (
