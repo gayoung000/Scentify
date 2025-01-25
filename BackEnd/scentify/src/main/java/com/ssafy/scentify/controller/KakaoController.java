2e50e770f83270c8a5ba81071e06d44342a2a353
SunJung <djtjswjd1@naver.com>
2025-01-25 16:59:50 +0900

    [CHORE] 카카오 컨트롤러 주석 추가

--- BackEnd/scentify/src/main/java/com/ssafy/scentify/controller/KakaoController.java
+++ BackEnd/scentify/src/main/java/com/ssafy/scentify/controller/KakaoController.java
@@ -55,11 +55,13 @@ public class KakaoController {
 	// API 5번 : 카카오 로그인
 	@GetMapping("/login")
 	public void kakaoLogin(HttpServletResponse response) {
+		// 카카오 리다이렉트 주소
 		String redirectUrl = "https://kauth.kakao.com/oauth/authorize"
 								+ "?client_id=" + kakaoApiKey
 								+ "&redirect_uri=" + kakaoRedirectUrl
 								+ "&response_type=code";
         try {
+        	// 리다이렉트
 			response.sendRedirect(redirectUrl);
 		} catch (IOException e) {
 			log.error("IOException: ", e);
@@ -70,42 +72,50 @@ public class KakaoController {
 	@GetMapping("/call-back")
 	public void kakaoCallback(@RequestParam("code") String code, HttpServletRequest request, HttpServletResponse response)  {
 	    try {
-		String[] tokens = kakaoService.getKakaoAccessToken(code);
-	    String refreshToken = tokens[0];
-	    String accessToken = tokens[1];
-	    String[] userInfo = kakaoService.getKakaoUserInfo(accessToken);
-	    
-		String id = userInfo[0];
-		String email = userInfo[1];
-		SocialLoginDto existingUserInfo = userService.getUserIdByEmail(email);
-		
-		//카카오에서 받은 이메일 정보로 가입한 회원이 이미 있는 경우
-		if (existingUserInfo != null && existingUserInfo.getSocialType() != 2) {
-			response.sendRedirect("http://localhost:3000/login/social?social=false"); 
-			return;
-		}
-
-	    if (existingUserInfo != null && existingUserInfo.getId().equals(id) ) { 
-	    	TokenDto tokenDto = tokenProvider.createJwtToken(existingUserInfo.getId());
-            tokenService.saveRefreshToken(existingUserInfo.getId(), tokenDto.getRefreshToken());
-            
-            Cookie accessTokenCookie = tokenProvider.createCookie(tokenDto.getAccessToken());
-            Cookie refreshTokenCookie = tokenProvider.createCookie(tokenDto.getRefreshToken());
-            response.addCookie(refreshTokenCookie);
-            
-			response.sendRedirect("http://localhost:3000/login/social?social=true&status=login");
-			return;
-	    }
-	    
-	    HttpSession session =  request.getSession();
-	    session.setAttribute("socialType", 2);
-	    session.setAttribute("id", id);
-		response.sendRedirect("http://localhost:3000/login/social?social=true&status=regist");
+	    	// 카카오에서 발급받은 토큰
+			String[] tokens = kakaoService.getKakaoAccessToken(code);
+		    String refreshToken = tokens[0];
+		    String accessToken = tokens[1];
+		    
+		    // 토큰을 이용해서 카카오에서 유저 정보 획득
+		    String[] userInfo = kakaoService.getKakaoUserInfo(accessToken);
+			String id = userInfo[0];
+			String email = userInfo[1];
+			
+			// 이메일 중복 확인
+			SocialLoginDto existingUserInfo = userService.getUserIdByEmail(email);
+			
+			// 해당 이메일 정보로 가입한 회원이 이미 있는데 카카오 소셜 회원이 아닌 경우
+			if (existingUserInfo != null && existingUserInfo.getSocialType() != 2) {
+				response.sendRedirect("http://localhost:3000/login/social?social=false"); 
+				return;
+			}
+			
+			// 해당 이메일 정보로 가입한 회원이 있고 아이디도 같은 경우 (소셜 로그인한 경우)
+		    if (existingUserInfo != null && existingUserInfo.getId().equals(id) ) { 
+		    	// 토큰 발급
+		    	TokenDto tokenDto = tokenProvider.createJwtToken(existingUserInfo.getId());
+	            tokenService.saveRefreshToken(existingUserInfo.getId(), tokenDto.getRefreshToken());
+	            
+	            // 발급한 토큰을 쿠키로 삽입
+	            Cookie accessTokenCookie = tokenProvider.createCookie(tokenDto.getAccessToken());
+	            Cookie refreshTokenCookie = tokenProvider.createCookie(tokenDto.getRefreshToken());
+	            response.addCookie(refreshTokenCookie);
+	            
+				response.sendRedirect("http://localhost:3000/login/social?social=true&status=login");
+				return;
+		    }
+		    
+		    // 가입이 필요한 경우 세션에 정보를 저장해준다
+		    HttpSession session =  request.getSession();
+		    session.setAttribute("socialType", 2);
+		    session.setAttribute("id", id);
+			response.sendRedirect("http://localhost:3000/login/social?social=true&status=regist");
 	    
 	    } catch (Exception e) {
 	    	try {
-				response.sendRedirect("http://localhost:3000");
-				
+	    		// 예기치 못한 에러 발생
+				response.sendRedirect("http://localhost:3000");			
 			} catch (IOException io) {
 				log.error("IOException: ", io);
 			}
@@ -116,12 +126,20 @@ public class KakaoController {
 	@PostMapping("/regist")
 	public ResponseEntity<?> kakaoRegist(@RequestBody SocialRegisterDto socialUser, HttpServletRequest request, HttpServletResponse response) {
 		try {
+			// 현재 사용자의 세션을 가져옴 (세션이 없는 경우 null)
 			HttpSession session = request.getSession(false);			
-			if (session == null) { return new ResponseEntity<>(HttpStatus.FORBIDDEN); }
 			
+			// 세션이 없는 경우
+			if (session == null) { return new ResponseEntity<>(HttpStatus.UNAUTHORIZED); }
+			
+			// 세션에서 아이디와 소셜 타입 정보 가져옴
 			String id = (String) session.getAttribute("id");
 			Integer socialType = (Integer) session.getAttribute("socialType");
 			
+			// 정보가 없는 경우
+			if (id == null || id.isEmpty() || socialType == null) { return new ResponseEntity<>(HttpStatus.UNAUTHORIZED); }
+			
+			// 데이터 유효성 검사
 			User user = new User();
 			user.setId(id);
 			user.setPassword(socialUser.getPassword());
@@ -132,12 +150,14 @@ public class KakaoController {
 			user.setGender(socialUser.getGender());
 			user.setBirth(socialUser.getBirth());
 			
+			// 유저 생성
 			userService.createUser(user);
-			return new ResponseEntity<>(HttpStatus.OK);
+			return new ResponseEntity<>(HttpStatus.OK);   // 성공적으로 처리됨
 	   
 		} catch (Exception e) {
+			// 예기치 못한 에러 처리
 			log.error("Exception: ", e);
-			 return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+			return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
 		}
 	}
 }
