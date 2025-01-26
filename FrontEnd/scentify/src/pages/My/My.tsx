f287073b2ef6ca5ebedf2cad80bf2561f10140f3
leegayoung <leegy0720@naver.com>
2025-01-26 16:31:31 +0900

    ✨ feat: S12P11A205-154 [user] 로그아웃 구현 <이가영>

--- FrontEnd/scentify/src/pages/My/My.tsx
+++ FrontEnd/scentify/src/pages/My/My.tsx
@@ -1,7 +1,22 @@
+import { Link, useNavigate } from 'react-router-dom';
+import { useAuthStore } from '../../stores/useAuthStore';
+
 const My = () => {
+  const logout = useAuthStore((state) => state.logout);
+  const navigate = useNavigate();
+
+  const handleLogout = async () => {
+    try {
+      await logout();
+      navigate('/auth/login'); // 로그아웃 후 로그인 페이지로 이동
+    } catch (error) {
+      console.error('로그아웃 실패:', error);
+    }
+  };
+
   return (
     <div className="flex h-full items-center justify-center">
-      <h1 className="text-3xl font-bold">My</h1>
+      <button onClick={handleLogout}>로그아웃</button>
     </div>
   );
 };
