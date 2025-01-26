a7551874845feb8235b75b84fe1f82d0c4bc273f
SunJung <djtjswjd1@naver.com>
2025-01-26 11:46:17 +0900

    [RENAME] 도메인 별로 디렉토리 구조 변경

---
+++ BackEnd/scentify/src/main/java/com/ssafy/scentify/user/controller/KakaoController.java
@@ -0,0 +1,163 @@
+package com.ssafy.scentify.user.controller;
+
+import java.io.IOException;
+import java.time.LocalTime;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.ssafy.scentify.auth.TokenService;
+import com.ssafy.scentify.auth.model.dto.TokenDto;
+import com.ssafy.scentify.common.util.TokenProvider;
+import com.ssafy.scentify.user.model.dto.UserDto.SocialLoginDto;
+import com.ssafy.scentify.user.model.dto.UserDto.SocialRegisterDto;
+import com.ssafy.scentify.user.model.entity.User;
+import com.ssafy.scentify.user.service.KakaoService;
+import com.ssafy.scentify.user.service.UserService;
+
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@RestController
+@RequestMapping("/v1/auth/kakao")
+public class KakaoController {
+	
+	@Value("${kakao.api-key}")
+	private String kakaoApiKey;
+
+	@Value("${kakao.redirect.url}")
+	private String kakaoRedirectUrl;
+	
+	private final UserService userService;
+	private final KakaoService kakaoService;
+	private final TokenProvider tokenProvider;
+	private final TokenService tokenService;
+
+	public KakaoController(UserService userService, KakaoService kakaoService, TokenProvider tokenProvider, TokenService tokenService) {
+		this.userService = userService;
+		this.kakaoService = kakaoService;
+		this.tokenProvider = tokenProvider;
+		this.tokenService = tokenService;
+	}
+	
+	// API 5번 : 카카오 로그인
+	@GetMapping("/login")
+	public void kakaoLogin(HttpServletResponse response) {
+		// 카카오 리다이렉트 주소
+		String redirectUrl = "https://kauth.kakao.com/oauth/authorize"
+								+ "?client_id=" + kakaoApiKey
+								+ "&redirect_uri=" + kakaoRedirectUrl
+								+ "&response_type=code";
+        try {
+        	// 리다이렉트
+			response.sendRedirect(redirectUrl);
+		} catch (IOException e) {
+			log.error("IOException: ", e);
+		}	
+	}
+	
+	// API 6번 : 카카오 콜백 메서드
+	@GetMapping("/call-back")
+	public void kakaoCallback(@RequestParam("code") String code, HttpServletRequest request, HttpServletResponse response)  {
+	    try {
+	    	// 카카오에서 발급받은 토큰
+			String[] tokens = kakaoService.getKakaoAccessToken(code);
+		    String refreshToken = tokens[0];
+		    String accessToken = tokens[1];
+		    
+		    // 토큰을 이용해서 카카오에서 유저 정보 획득
+		    String[] userInfo = kakaoService.getKakaoUserInfo(accessToken);
+			String id = userInfo[0];
+			String email = userInfo[1];
+			
+			// 이메일 중복 확인
+			SocialLoginDto existingUserInfo = userService.getUserIdByEmail(email);
+			
+			// 해당 이메일 정보로 가입한 회원이 이미 있는데 카카오 소셜 회원이 아닌 경우
+			if (existingUserInfo != null && existingUserInfo.getSocialType() != 2) {
+				response.sendRedirect("http://localhost:3000/login/social?social=false"); 
+				return;
+			}
+			
+			// 해당 이메일 정보로 가입한 회원이 있고 아이디도 같은 경우 (소셜 로그인한 경우)
+		    if (existingUserInfo != null && existingUserInfo.getId().equals(id) ) { 
+		    	// 토큰 발급
+		    	TokenDto tokenDto = tokenProvider.createJwtToken(existingUserInfo.getId());
+	            tokenService.saveRefreshToken(existingUserInfo.getId(), tokenDto.getRefreshToken());
+	            
+	            // 발급한 토큰을 쿠키로 삽입
+	            Cookie accessTokenCookie = tokenProvider.createCookie(tokenDto.getAccessToken());
+	            Cookie refreshTokenCookie = tokenProvider.createCookie(tokenDto.getRefreshToken());
+	            response.addCookie(refreshTokenCookie);
+	            
+				response.sendRedirect("http://localhost:3000/login/social?social=true&status=login");
+				return;
+		    }
+		    
+		    // 가입이 필요한 경우 세션에 정보를 저장해준다
+		    HttpSession session =  request.getSession();
+		    session.setAttribute("socialType", 2);
+		    session.setAttribute("id", id);
+			response.sendRedirect("http://localhost:3000/login/social?social=true&status=regist");
+	    
+	    } catch (Exception e) {
+	    	try {
+	    		// 예기치 못한 에러 발생
+				response.sendRedirect("http://localhost:3000");			
+			} catch (IOException io) {
+				log.error("IOException: ", io);
+			}
+	    } 
+	}
+	
+	// API 7번 : 카카오 회원가입
+	@PostMapping("/regist")
+	public ResponseEntity<?> kakaoRegist(@RequestBody SocialRegisterDto socialUser, HttpServletRequest request, HttpServletResponse response) {
+		try {
+			// 현재 사용자의 세션을 가져옴 (세션이 없는 경우 null)
+			HttpSession session = request.getSession(false);			
+			
+			// 세션이 없는 경우
+			if (session == null) { return new ResponseEntity<>(HttpStatus.UNAUTHORIZED); }
+			
+			// 세션에서 아이디와 소셜 타입 정보 가져옴
+			String id = (String) session.getAttribute("id");
+			Integer socialType = (Integer) session.getAttribute("socialType");
+			
+			// 정보가 없는 경우
+			if (id == null || id.isEmpty() || socialType == null) { return new ResponseEntity<>(HttpStatus.UNAUTHORIZED); }
+			
+			// 데이터 유효성 검사
+			User user = new User();
+			user.setId(id);
+			user.setPassword(socialUser.getPassword());
+			user.setNickname(socialUser.getNickname());
+			user.setEmail(socialUser.getEmail());
+			user.setImgNum(socialUser.getImgNum());
+			user.setSocialType(socialType);
+			user.setGender(socialUser.getGender());
+			user.setBirth(socialUser.getBirth());
+			
+			// 유저 생성
+			userService.createUser(user);
+			return new ResponseEntity<>(HttpStatus.OK);   // 성공적으로 처리됨
+	   
+		} catch (Exception e) {
+			// 예기치 못한 에러 처리
+			log.error("Exception: ", e);
+			return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+		}
+	}
+}
