2f115f252848c5047869bf2e2ceb5d1c2d6b98e6
SunJung <djtjswjd1@naver.com>
2025-01-26 12:52:18 +0900

    [DEBUG] 레파지토리 스캔 경로 수정

--- BackEnd/scentify/src/main/java/com/ssafy/scentify/ScentifyApplication.java
+++ BackEnd/scentify/src/main/java/com/ssafy/scentify/ScentifyApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
 @SpringBootApplication(exclude = SecurityAutoConfiguration.class)
-@MapperScan("com.ssafy.scentify.model.repository")
+@MapperScan("com.ssafy.scentify")
 public class ScentifyApplication {
 
 	public static void main(String[] args) {
