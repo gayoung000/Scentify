7650119c5ddbaa38f5141e83e5933d2d50a85341
seof622 <seof622@naver.com>
2025-02-03 13:56:24 +0900

    feat: S12P11A205-254 add Mqtt client for raspberry pi
    Former-commit-id: 5a8158da0197392a51f8222110f035f87957f51c

---
+++ Embedded/Pi/mqtt_client.py
@@ -0,0 +1,96 @@
+import asyncio
+import aiomqtt
+import time
+
+class MQTTClient:
+    _instance = None  
+
+    def __new__(cls, *args, **kwargs):
+        """싱글톤 인스턴스를 생성 및 반환"""
+        if cls._instance is None:
+            cls._instance = super(MQTTClient, cls).__new__(cls)
+            cls._instance.__initialized = False  
+        return cls._instance
+    
+    def __init__(self, url, work_queue):
+        """초기화 (중복 실행 방지)"""
+        if not self.__initialized:
+            self.__initialized = True  
+            self.__url = url
+            self.device_id_list = [1]
+            self.client = None
+            self.work_queue = work_queue
+
+    async def connect(self):
+        async with aiomqtt.Client(self.__url) as client:
+            self.client = client
+            await self.subscribe()
+
+            asyncio.create_task(self.listen_message())
+            print("Complete Connect!")
+            while True:
+                await asyncio.sleep(1)
+    
+    async def listen_message(self):
+        async for message in self.client.messages:
+            await self.on_message(message)
+
+    async def on_message(self, message):
+        topic = message.topic
+        payload = message.payload.decode()
+
+        print(f"메시지 수신: {topic} -> {payload}")
+
+        await self.process_message(topic, payload)
+
+    async def process_message(self, topic, payload):
+        message = {
+            "topic" : "",
+            "payload" : payload,
+        }
+        if topic == f"{self.device_id_list[0]}/Status/Remainder":
+            message["topic"] = "DeviceStatus/Capsule/Remainder"
+        elif topic == f"{self.device_id_list[0]}/Status/DetectionResult":
+            message["payload"] = None
+            # 사람 단순 감지
+            if payload == 1:
+                message["topic"] = "DeviceStatus/Camera/SimpleDetection"
+            # 운동 중
+            elif payload == 2: 
+                message["topic"] = "DeviceStatus/Camera/UserAction/Exercise"
+            # 휴식 중
+            elif payload == 3:
+                message["topic"] = "DeviceStatus/Camera/UserAction/Focus" 
+
+        elif topic == f"{self.device_id_list[0]}/Status/Stink":
+            message["payload"] = None
+            message["topic"] = "DeviceStatus/Sensor/Stink"
+        elif topic == f"{self.device_id_list[0]}/Setting":
+            pass
+
+        await self.work_queue.put(message)
+
+    async def subscribe(self):
+        if self.client is not None:
+            await self.client.subscribe(f"{self.device_id_list[0]}/Status/Remainder")
+            await self.client.subscribe(f"{self.device_id_list[0]}/Status/DetectionResult")
+            await self.client.subscribe(f"{self.device_id_list[0]}/Status/Stink")
+            await self.client.subscribe(f"{self.device_id_list[0]}/Setting")
+            print("Complete Subscribe!")
+
+    async def publish(self, topic, payload):
+        if self.client is None:
+            print("MQTT 클라이언트가 연결되지 않았습니다!")
+            return
+        await self.client.publish(topic, payload)
+        print(f"pub! topic : {topic}, payload : {payload}")
+
+    
+if __name__ == "__main__":
+    queue = asyncio.Queue()
+    mqtt_client = MQTTClient("localhost", queue)
+    asyncio.run(mqtt_client.connect())
+    while True:
+        data = asyncio.run(queue.get())
+        print(f"Data is {data}!!\n")
+        time.sleep(1)
