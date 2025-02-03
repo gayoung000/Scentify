f2415394bc9195c8c1c6f9ca0ad7c94c3bb7b3b8
어선정 <djtjswjd1@naver.com>
2025-02-03 20:52:28 +0900

    Update BackEnd/scentify/src/main/java/com/ssafy/scentify/user/model/repository/UserSecuInfoRepository.java

--- BackEnd/scentify/src/main/java/com/ssafy/scentify/user/model/repository/UserSecuInfoRepository.java
+++ BackEnd/scentify/src/main/java/com/ssafy/scentify/user/model/repository/UserSecuInfoRepository.java
@@ -11,14 +11,14 @@ import com.ssafy.scentify.user.model.entity.UserSecuInfo;
 public interface UserSecuInfoRepository {
 
 	// userSecuInfo 삽입
-	@Insert("INSERT INTO UserSecuInfo (user_id, salt) VALUES (#{userId}, #{salt})")
+	@Insert("INSERT INTO usersecuinfo (user_id, salt) VALUES (#{userId}, #{salt})")
 	boolean createSecuInfo(UserSecuInfo secuInfo);
 	
 	// userId로 레코드 찾기
-	@Select("SELECT * FROM UserSecuInfo WHERE user_id = #{userId}")
+	@Select("SELECT * FROM usersecuinfo WHERE user_id = #{userId}")
 	UserSecuInfo getSecuInfoById(String userId);
 	
 	// userId에 해당하는 유저의 salt 업데이트
-	@Update("UPDATE UserSecuInfo SET salt = #{salt} WHERE user_id = #{userId}")
+	@Update("UPDATE usersecuinfo SET salt = #{salt} WHERE user_id = #{userId}")
 	boolean updateSalt(String userId, String salt);
 }
