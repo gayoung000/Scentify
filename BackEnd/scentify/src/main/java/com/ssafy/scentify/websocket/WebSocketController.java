c09478cc8aec7e27f1bba193cffd17219aa59fec
SunJung <djtjswjd1@naver.com>
2025-02-06 08:27:28 +0900

    [CHORE] 메서드 이름 수정

--- BackEnd/scentify/src/main/java/com/ssafy/scentify/websocket/WebSocketController.java
+++ BackEnd/scentify/src/main/java/com/ssafy/scentify/websocket/WebSocketController.java
@@ -297,7 +297,7 @@ public class WebSocketController {
 	
 	// API 41번 : 탈취 모드 향기 정보 요청
 	@MessageMapping("/DeviceStatus/Sensor")
-	public void sendStinkCombination(@Payload CombinationRequest request) {
+	public void sendCombination(@Payload CombinationRequest request) {
 		String token = request.getToken();
 	    Integer id = null;
 
