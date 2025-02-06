10c162c883f981a9388d1b3d0a15fb88c12925b3
SunJung <djtjswjd1@naver.com>
2025-02-06 15:27:09 +0900

    [FEAT] S12P11A205-252 찜 삭제 기능 구현

--- BackEnd/scentify/src/main/java/com/ssafy/scentify/favorite/model/repository/FavoriteRepository.java
+++ BackEnd/scentify/src/main/java/com/ssafy/scentify/favorite/model/repository/FavoriteRepository.java
@@ -1,12 +1,18 @@
 package com.ssafy.scentify.favorite.model.repository;
 
+import org.apache.ibatis.annotations.Delete;
 import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
 
 @Mapper
 public interface FavoriteRepository {
 	
+	// 찜 추가
 	@Insert("INSERT INTO favorite (user_id, combination_id) VALUES (#{userId}, #{combinationId})")
 	boolean addCombinationToFavorites(String userId, int combinationId);
+	
+	// 찜 삭제
+	@Delete("DELETE FROM favorite WEHRE id = #{id}, user_id = #{userId}")
+	boolean deleteFavorite(int id, String userId);
 	
 }
