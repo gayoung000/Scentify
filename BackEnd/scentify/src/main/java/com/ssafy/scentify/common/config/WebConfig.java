eccaf40df0dbc9a72b230dee38e6868f9e13a758
SunJung <djtjswjd1@naver.com>
2025-02-08 21:46:29 +0900

    [CHORE] nginx 사용으로 cors 주석 처리

--- BackEnd/scentify/src/main/java/com/ssafy/scentify/common/config/WebConfig.java
+++ BackEnd/scentify/src/main/java/com/ssafy/scentify/common/config/WebConfig.java
@@ -6,13 +6,13 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/v1/**")
-                .allowedOrigins("http://localhost:5173")
-                .allowedMethods("GET", "POST")  
-                .allowedHeaders("Authorization", "Content-Type")
-                .allowCredentials(true); 
-    }
+//    @Override
+//    public void addCorsMappings(CorsRegistry registry) {
+//        registry.addMapping("/v1/**")
+//                .allowedOrigins("http://localhost:5173")
+//                .allowedMethods("GET", "POST")  
+//                .allowedHeaders("Authorization", "Content-Type")
+//                .allowCredentials(true); 
+//    }
 }
 
