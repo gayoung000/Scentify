e769532189fee9410bd3d757970a850db3bf806c
seof622 <seof622@naver.com>
2025-02-11 07:59:02 +0900

    feat: complete stink sensor operation test
    Former-commit-id: cfb1d949a3e2d8c2a20c143017e7ab34bff39ab2

--- Embedded/HW/test/Sensor/GSAT-P110.py
+++ Embedded/HW/test/Sensor/GSAT-P110.py
@@ -1,26 +1,100 @@
-import spidev
 import time
 
-spi = spidev.SpiDev()
-spi.open(0, 0)  
-spi.max_speed_hz = 10000
+import Jetson.GPIO as GPIO
 
-def read_adc0834(channel):
-    if channel < 0 or channel > 3:
-        raise ValueError("Channel must be 0-3")
-    
-    start_bit = 1
-    single_ended = 1
-    config_bits = (single_ended << 2) | (channel >> 1)
-    command = [start_bit, (config_bits << 4) | ((channel & 1) << 3)]
-    
-    adc_response = spi.xfer2(command)
-    adc_value = ((adc_response[0] & 0x01) << 7) | (adc_response[1] >> 1)
-    return adc_value
 
-channel = 0
-while True:
-    value = read_adc0834(channel)
-    voltage = (value / 255.0) * 3.3  
-    print(f"ADC Value: {value}, Voltage: {voltage:.2f}V")
-    time.sleep(0.5)
+class ADC0834:
+    def __init__(self, cs: int, clk: int, dio: int, frequency: int = 50_000) -> None:
+        self.cs = cs
+        self.clk = clk
+        self.dio = dio
+        self.frequency = frequency
+        GPIO.setmode(GPIO.BOARD)
+        GPIO.setup(self.cs, GPIO.OUT)
+        GPIO.setup(self.clk, GPIO.OUT)
+
+    def read(self, channel: int = 0) -> int:
+        """
+        Read the value from the specified channel.
+        Returns an int between 0 and 255.
+        """
+        # Set CS pin to low to enable the ADC
+        GPIO.output(self.cs, 0)
+
+        # Set DIO pin to output to setup the ADC to read from the specified channel
+        GPIO.setup(self.dio, GPIO.OUT)
+
+        # Start bit
+        self._set_clock_low()
+        GPIO.output(self.dio, 1)
+        self._set_clock_high()
+
+        # SGL/DIF
+        self._set_clock_low()
+        GPIO.output(self.dio, 1)
+        self._set_clock_high()
+
+        # ODD/SIGN
+        self._set_clock_low()
+        GPIO.output(self.dio, channel % 2)
+        self._set_clock_high()
+
+        # SELECT1
+        self._set_clock_low()
+        GPIO.output(self.dio, int(channel > 1))
+        self._set_clock_high()
+
+        # Allow the MUX to settle for 1/2 clock cycle
+        self._set_clock_low()
+
+        # Switch DIO pin to input to read data
+        GPIO.setup(self.dio, GPIO.IN)
+
+        # Read data from MSB to LSB
+        val1 = 0
+        for i in range(0, 8):
+            self._set_clock_high()
+            self._set_clock_low()
+            val1 = val1 << 1
+            val1 = val1 | GPIO.input(self.dio)
+
+        # Read data from LSB to MSB
+        val2 = 0
+        for i in range(0, 8):
+            bit = GPIO.input(self.dio) << i
+            val2 = val2 | bit
+            self._set_clock_high()
+            self._set_clock_low()
+
+        # Set CS pin to high to clear all internal registers
+        GPIO.output(self.cs, 1)
+
+        # Done reading, set DIO pin back to output
+        GPIO.setup(self.dio, GPIO.OUT)
+
+        # Compare the two values to ensure they match
+        if val1 == val2:
+            return val1
+        else:
+            return 0
+
+    def _set_clock_high(self):
+        GPIO.output(self.clk, GPIO.HIGH)
+        self._tick()
+
+    def _set_clock_low(self):
+        GPIO.output(self.clk, GPIO.LOW)
+        self._tick()
+
+    def _tick(self):
+        period = 1 / self.frequency
+        period_half = period / 2
+        time.sleep(period_half)
+
+
+if __name__ == "__main__":
+    adc = ADC0834(24, 23, 19)
+    while True:
+        value = adc.read()
+        print(value)
+        time.sleep(1)
