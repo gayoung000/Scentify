a750f7147f413c5f5382279acdbf91fc8a8b25fa
SunJung <djtjswjd1@naver.com>
2025-02-12 09:30:31 +0900

    [CHORE] 웹소켓 관련 주석 해제

--- BackEnd/scentify/src/main/java/com/ssafy/scentify/favorite/FavoriteController.java
+++ BackEnd/scentify/src/main/java/com/ssafy/scentify/favorite/FavoriteController.java
@@ -18,7 +18,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.ssafy.scentify.combination.CombinationController;
 import com.ssafy.scentify.combination.CombinationService;
 import com.ssafy.scentify.combination.model.dto.CombinationDto;
 import com.ssafy.scentify.common.service.AIService;
