11708da12069318804117e1b99df89b17d514f92
leegayoung <leegy0720@naver.com>
2025-02-15 10:53:33 +0900

    🐛  fix: [home] 기본향 등록 시 새로운 분사 최대 횟수에 맞춰 count 조정 <이가영>

--- FrontEnd/scentify/src/feature/Home/defaultscent/SpaceDescription.tsx
+++ FrontEnd/scentify/src/feature/Home/defaultscent/SpaceDescription.tsx
@@ -5,13 +5,15 @@ function SpaceDescription() {
         <h2 className="text-lightgray mb-1">잠깐! 공간 크기는요..</h2>
         <div className="px-4 py-2 bg-white w-[320px] h-[99px] rounded-lg">
           <div className="mb-3">
-            <h3 className="text-gray mb-1">소형 공간: 10평 이하 (작은 방)</h3>
-            <p className="text-gray">• 작은 방, 욕실, 개인 서재, 소규모 침실</p>
+            <h3 className="text-gray mb-1">소형 공간: 10평 이하</h3>
+            <p className="text-gray">
+              • 예시 : 작은 방, 욕실, 개인 서재, 소규모 침실
+            </p>
           </div>
 
           <div className="">
-            <h3 className="text-gray mb-1">중/대 공간: 11평 ~ 25평 (거실)</h3>
-            <p className="text-gray">• 거실, 중형 침실, 원룸/투룸</p>
+            <h3 className="text-gray mb-1">중/대 공간: 11평 ~ 25평</h3>
+            <p className="text-gray">• 예시 : 거실, 중형 침실, 원룸/투룸</p>
           </div>
         </div>
       </div>
